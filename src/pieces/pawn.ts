5d06967f466680c80f426ad1278ec6a0b7f4cd54
Shlomi Nugarker <shlomin1231@gmail.com>
2023-09-23 19:10:55 +0300

    getAllPossibleCoordsPawn

--- src/pieces/pawn.ts
+++ src/pieces/pawn.ts
@@ -1,3 +1,4 @@
+import { Game } from '..'
 import { Coord } from '../interfaces/Coord'
 import { Piece } from './piece'
 
@@ -5,15 +6,111 @@ import { Piece } from './piece'
 export class P extends Piece {
   name = 'P'
   shape = '♙'
-  constructor(coord: Coord) {
+  game: Game
+
+  constructor(game: Game, coord: Coord) {
     super(coord)
+    this.game = game
   }
+
+  getPossibleCoords = (): Coord[] => getAllPossibleCoordsPawn(this)
 }
 // PAWN_BLACK:
 export class p extends Piece {
   name = 'p'
   shape = '♟'
-  constructor(coord: Coord) {
+  game: Game
+
+  constructor(game: Game, coord: Coord) {
     super(coord)
+    this.game = game
   }
+  getPossibleCoords = (): Coord[] => getAllPossibleCoordsPawn(this)
+}
+
+function getAllPossibleCoordsPawn(self: P | p) {
+  const board = self.game.board.board
+  const isWhite = self.game.isBlackPiece(self.name)
+  const pieceCoord = self.coord
+
+  const res: { i: number; j: number }[] = []
+
+  // Regular steps
+  let diff = isWhite ? -1 : 1
+  let nextCoord = { i: pieceCoord.i + diff, j: pieceCoord.j }
+
+  if (self.game.isEmptyCell(nextCoord)) {
+    res.push(nextCoord)
+
+    if ((pieceCoord.i === 1 && !isWhite) || (pieceCoord.i === 6 && isWhite)) {
+      diff *= 2
+      nextCoord = { i: pieceCoord.i + diff, j: pieceCoord.j }
+      if (self.game.isEmptyCell(nextCoord)) res.push(nextCoord)
+    }
+  }
+
+  // eatable cells:
+  if (isWhite) {
+    const nextLeftCoord = { i: pieceCoord.i - 1, j: pieceCoord.j - 1 }
+    const nextRightCoord = { i: pieceCoord.i - 1, j: pieceCoord.j + 1 }
+
+    if (
+      board[nextLeftCoord.i][nextLeftCoord.j] &&
+      !self.game.isColorPieceWorthCurrPlayerColor(
+        board[nextLeftCoord.i][nextLeftCoord.j]
+      )
+    ) {
+      res.push(nextLeftCoord)
+    }
+    if (
+      board[nextRightCoord.i][nextRightCoord.j] &&
+      !self.game.isColorPieceWorthCurrPlayerColor(
+        board[nextRightCoord.i][nextRightCoord.j]
+      )
+    ) {
+      res.push(nextRightCoord)
+    }
+  }
+  if (!isWhite) {
+    const nextLeftCoord = { i: pieceCoord.i + 1, j: pieceCoord.j - 1 }
+    const nextRightCoord = { i: pieceCoord.i + 1, j: pieceCoord.j + 1 }
+
+    if (
+      board[nextLeftCoord.i][nextLeftCoord.j] &&
+      !self.game.isColorPieceWorthCurrPlayerColor(
+        board[nextLeftCoord.i][nextLeftCoord.j]
+      )
+    ) {
+      res.push(nextLeftCoord)
+    }
+    if (
+      board[nextRightCoord.i][nextRightCoord.j] &&
+      !self.game.isColorPieceWorthCurrPlayerColor(
+        board[nextRightCoord.i][nextRightCoord.j]
+      )
+    ) {
+      res.push(nextRightCoord)
+    }
+  }
+
+  // Check if can eat cell after 2 steps of pawn:
+  if (
+    self.game.eatableCellAfterTwoStepsPawnWhite &&
+    self.game.isBlackTurn &&
+    pieceCoord.i === 4
+  ) {
+    const eatableCell = { ...self.game.eatableCellAfterTwoStepsPawnWhite }
+    eatableCell.i = eatableCell.i + 1
+    res.push(eatableCell)
+  } else if (
+    self.game.eatableCellAfterTwoStepsPawnBlack &&
+    !self.game.isBlackTurn &&
+    pieceCoord.i === 3
+  ) {
+    const eatableCell = { ...self.game.eatableCellAfterTwoStepsPawnBlack }
+    eatableCell.i = eatableCell.i - 1
+    res.push(eatableCell)
+  }
+
+  return res
 }
